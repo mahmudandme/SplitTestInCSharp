b0be0df72ed2d36b211fe75faae3dbc07ad58a7f
mahmudandme <mailtommrr@gmail.com>
2015-07-16 20:39:22 +0600

    Commited first time

---
+++ SplitTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SplitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //string[] year = commaDelimited.Split(new char[] { ',' });
+
+            string str = "Mahmud|Rahman";
+
+            string[] str1 = str.Split(new char[]{'|'});
+            Console.WriteLine(str1[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
